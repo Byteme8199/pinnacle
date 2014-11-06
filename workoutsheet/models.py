e543395fc63f9eb3835b9f30a5ffeffb36c2193d
Ryan Gordon <ryan@hdvideoandwebdesign.com>
2014-11-06 02:39:48 +0000

    [update] project cleanup

--- workoutsheet/models.py
+++ workoutsheet/models.py
@@ -85,7 +85,7 @@ class WorkoutSheet(models.Model):
 
 	def fullsheet(self):
 		fullsheet = []
-		for week in WorkoutWeek.objects.filter(workout=self.pk).order_by('group_order', 'group'):	
+		for week in WorkoutWeek.objects.filter(workout=self.pk).order_by('group', 'group_order'):	
 			fullsheet.append(week)
 		return fullsheet
 	
